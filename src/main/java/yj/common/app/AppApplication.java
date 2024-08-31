1ffcc4f939557cba333f6104b6613916747dd575
韩玉坤 <2059416370@qq.com>
2024-08-31 13:53:01 +0800

    redis

--- src/main/java/yj/common/app/AppApplication.java
+++ src/main/java/yj/common/app/AppApplication.java
@@ -2,13 +2,6 @@ package yj.common.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import yj.common.app.entity.student.Student;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class AppApplication {
